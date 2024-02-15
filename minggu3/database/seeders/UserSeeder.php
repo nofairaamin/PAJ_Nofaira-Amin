47e74c2944886f0d98197aab8f894bf92f977c64
nofa paj <nofairaamin1312@gmail.com>
2024-02-15 08:11:38 +0700

    commit ke2

---
+++ minggu3/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facedes\DB;
+// use Illuminate\Support\Facedes\Hash;
+// use Illuminate\Support\Str;
+use DB;
+use Hash;
+use Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name'=>Str::random(10),
+            'email'=>Str::random(10).'@gmail.com',
+            'password'=>Hash::make('password'),
+        ]);
+    }
+}
